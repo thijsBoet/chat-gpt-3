8247d72dd95973b4f3d081f463a5ed8958b89995
thijsBoet <m.boet@chello.nl>
2023-01-17 17:54:24 +0100

    changed fetch from localhost to https://chat-gpt-3-osfm.onrender.com/

--- client/script.js
+++ client/script.js
@@ -67,7 +67,7 @@ async function handleSubmit(event) {
 
 	loader(messageDiv);
 
-	const response = await fetch('http://localhost:5000', {
+	const response = await fetch('https://chat-gpt-3-osfm.onrender.com/', {
 		method: 'POST',
 		headers: {
 			'Content-Type': 'application/json',
